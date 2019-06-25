a1cf7f74ae594e404c75a01416a4c3741aefb013
Thomas Moffet <pantherman1996@gmail.com>
2019-06-25 18:47:37 +1000

    Multithreading; seems to not peak the CPU very much but it improves performance a tonne.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+
+#include <thread>
+#include <mutex>
+#include <vector>
 #include <SDL.h>
 
 #include "MathFunctions.h"
@@ -139,45 +143,57 @@ void ShowBuffer(SDL_Renderer* renderer, SDL_Texture* texture) {
 /// <param name="renderer"></param>
 /// <param name="texture"></param>
 void DrawSphereScene(SDL_Renderer* renderer, SDL_Texture* texture) {
-	// The field of view in degrees. This is the angle between the highest and lowest rays on screen.
-	constexpr Degrees vFov = 75.0;
-	// The aspect ratio of the display in width:height ratio.
-	constexpr double aspectRatio = static_cast<double>(WINDOW_WIDTH) / WINDOW_HEIGHT;
-	// The field of view horizontally. This is calculated based on the vertical fov and the aspect ratio.
-	constexpr Degrees hFov = vFov * aspectRatio;
+	std::vector<std::thread> threads;
+	std::mutex sdlLock;
+	for (int threadNum = 0; threadNum < static_cast<int>(std::thread::hardware_concurrency()); ++threadNum) {
+		threads.emplace_back([renderer, texture, threadNum, &sdlLock]() {
+			// The field of view in degrees. This is the angle between the highest and lowest rays on screen.
+			constexpr Degrees vFov = 75.0;
+			// The aspect ratio of the display in width:height ratio.
+			constexpr double aspectRatio = static_cast<double>(WINDOW_WIDTH) / WINDOW_HEIGHT;
+			// The field of view horizontally. This is calculated based on the vertical fov and the aspect ratio.
+			constexpr Degrees hFov = vFov * aspectRatio;
 
-	for (int screenY = 0; screenY < WINDOW_HEIGHT; ++screenY) {
-		// The gradient of the Y axis of the ray (i.e. number of Y units moved per Z unit).
-		double yGradient = std::tan(ToRadians(Lerp(vFov / 2.0, vFov / -2.0, ((screenY + 1) / (2.0 * WINDOW_HEIGHT)))));
-		for (int screenX = 0; screenX < WINDOW_WIDTH; ++screenX) {
-			double xGradient = std::tan(ToRadians(Lerp(hFov / -2.0, hFov / 2.0, ((screenX + 1) / (2.0 * WINDOW_WIDTH)))));
-			// Now to calculate the intersection point of the line and the sphere.
-			// This can be solved with some algerbraic manipulation and the quadratic formula.
-			// You get no solution if the sphere contains the origin. Otherwise you're likely to get two solutions,
-			// the lower value will be the closer one, and therefore the one you see.
-			double quadPart1 = -(2 * xGradient * sphereX - 2 * yGradient * sphereY - 2 * sphereZ);
-			double quadPart2 = 4 * (xGradient * xGradient + yGradient * yGradient + 1) * (sphereX * sphereX + sphereY * sphereY + sphereZ * sphereZ - sphereR * sphereR);
-			double quadPart3 = 2 * (xGradient * xGradient + yGradient * yGradient + 1);
-			if (quadPart2 < 0.0) {
-				continue;
+			for (int screenY = threadNum; screenY < WINDOW_HEIGHT; screenY += static_cast<int>(std::thread::hardware_concurrency())) {
+				// The gradient of the Y axis of the ray (i.e. number of Y units moved per Z unit).
+				double yGradient = std::tan(ToRadians(Lerp(vFov / 2.0, vFov / -2.0, ((screenY + 1) / (2.0 * WINDOW_HEIGHT)))));
+				for (int screenX = 0; screenX < WINDOW_WIDTH; ++screenX) {
+					double xGradient = std::tan(ToRadians(Lerp(hFov / -2.0, hFov / 2.0, ((screenX + 1) / (2.0 * WINDOW_WIDTH)))));
+					// Now to calculate the intersection point of the line and the sphere.
+					// This can be solved with some algerbraic manipulation and the quadratic formula.
+					// You get no solution if the sphere contains the origin. Otherwise you're likely to get two solutions,
+					// the lower value will be the closer one, and therefore the one you see.
+					double quadPart1 = -(2 * xGradient * sphereX - 2 * yGradient * sphereY - 2 * sphereZ);
+					double quadPart2 = 4 * (xGradient * xGradient + yGradient * yGradient + 1) * (sphereX * sphereX + sphereY * sphereY + sphereZ * sphereZ - sphereR * sphereR);
+					double quadPart3 = 2 * (xGradient * xGradient + yGradient * yGradient + 1);
+					if (quadPart2 < 0.0) {
+						continue;
+					}
+					double z1 = (quadPart1 + std::sqrt(quadPart2)) / quadPart3;
+					double z2 = (quadPart1 - std::sqrt(quadPart2)) / quadPart3;
+					if (IsBetween(z1, z2, 0.0)) {
+						continue;
+					}
+					double z = /*Min(z1, z2)*/z1;
+					double x = xGradient * z;
+					double y = yGradient * z;
+
+					// Now determine the brightness. We use the cosine rule to give us the angle, and then determine the
+					// brightness from that.
+					double dotProduct = (sphereX - x) * sphereX + (sphereY - y) * sphereY + (sphereZ - z) * sphereZ;
+					double cosineValue = dotProduct / Pythagoras(sphereX, sphereY, sphereZ) / sphereR;
+					double brightness = 0.5 * Clamp(0.0, 1.0, cosineValue) + 0.5;
+
+					sdlLock.lock();
+					SDL_SetRenderDrawColor(renderer, static_cast<Uint8>(brightness * 255), static_cast<Uint8>(brightness * 255), static_cast<Uint8>(brightness * 255), SDL_ALPHA_OPAQUE);
+					SDL_RenderDrawPoint(renderer, screenX, screenY);
+					sdlLock.unlock();
+				}
 			}
-			double z1 = (quadPart1 + std::sqrt(quadPart2)) / quadPart3;
-			double z2 = (quadPart1 - std::sqrt(quadPart2)) / quadPart3;
-			if (IsBetween(z1, z2, 0.0)) {
-				continue;
-			}
-			double z = /*Min(z1, z2)*/z1;
-			double x = xGradient * z;
-			double y = yGradient * z;
-
-			// Now determine the brightness. We use the cosine rule to give us the angle, and then determine the
-			// brightness from that.
-			double dotProduct = (sphereX - x) * sphereX + (sphereY - y) * sphereY + (sphereZ - z) * sphereZ;
-			double cosineValue = dotProduct / Pythagoras(sphereX, sphereY, sphereZ) / sphereR;
-			double brightness = 0.5 * Clamp(0.0, 1.0, cosineValue) + 0.5;
-
-			SDL_SetRenderDrawColor(renderer, static_cast<Uint8>(brightness * 255), static_cast<Uint8>(brightness * 255), static_cast<Uint8>(brightness * 255), SDL_ALPHA_OPAQUE);
-			SDL_RenderDrawPoint(renderer, screenX, screenY);
-		}
+		});
+	}
+	for (int threadNum = 0; threadNum < static_cast<int>(std::thread::hardware_concurrency()); ++threadNum) {
+		threads[threadNum].join();
 	}
 }
+
